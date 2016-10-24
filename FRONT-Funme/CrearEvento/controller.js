2db399c11d714c9853ed420577e616aac7282424
virgi680 <virgi680@gmail.com>
2016-10-24 12:50:04 +0200

    Pantalla crear evento y registro

--- FRONT-Funme/CrearEvento/controller.js
+++ FRONT-Funme/CrearEvento/controller.js
@@ -4,10 +4,6 @@
 'use strict';
 var app = angular.module('app', [])
 app.controller('controlador', ['$scope','$http', function($scope, $http) {
-					$scope.newPage = function() {
-						open('crearEvento.html',
-								'top=300,left=300,width=300,height=300');
-					}
 					$scope.cancelar = function() {
 						open('Calendario.html');
 					}
@@ -15,25 +11,25 @@ app.controller('controlador', ['$scope','$http', function($scope, $http) {
 						window.close();
 					};
 					$scope.insertEvent = function() {
-
-						var insertarEv = {
-								lugar : $scope.lugar,
-								nombre : $scope.nombre
-							};
-						var mes = $scope.dia.getMonth()+1;
-						insertarEv.dia = $scope.dia.getDate() + "/" + mes + "/" + $scope.dia.getFullYear(); 
-						insertarEv.hora = $scope.hora.getHours() + ":" + $scope.hora.getMinutes();
-						var posicion_x; 
-						var posicion_y; 
-						posicion_x=(screen.width/2)-(400/2); 
-						posicion_y=(screen.height/2)-(300/2); 
 						//alert(insertarEv.hora);							
-			
-						$http.post('http://localhost:8080/funme/crearEvento',insertarEv)
-						.success(function(data) {
-							window.open('popup-exito.html', this.target, 'width=400,height=300,left='+posicion_x+',top='+posicion_y+'')
-						}).error(function(data) {
-							window.open('popup-exito.html', this.target, 'width=400,height=300,left='+posicion_x+',top='+posicion_y+'')
-						});
+						if($scope.crearEvento.$valid){
+							var insertarEv = {
+									lugar : $scope.lugar,
+									nombre : $scope.nombre
+								};
+							var mes = $scope.dia.getMonth()+1;
+							insertarEv.dia = $scope.dia.getDate() + "/" + mes + "/" + $scope.dia.getFullYear(); 
+							insertarEv.hora = $scope.hora.getHours() + ":" + $scope.hora.getMinutes();
+							var posicion_x; 
+							var posicion_y; 
+							posicion_x=(screen.width/2)-(400/2); 
+							posicion_y=(screen.height/2)-(300/2); 
+							$http.post('http://localhost:8080/funme/crearEvento',insertarEv)
+							.success(function(data) {
+								window.open('popup-exito.html', this.target, 'width=400,height=300,left='+posicion_x+',top='+posicion_y+'')
+							}).error(function(data) {
+								window.open('popup-exito.html', this.target, 'width=400,height=300,left='+posicion_x+',top='+posicion_y+'')
+							});
+						}
 					}
 				} ]);
--- FRONT-Funme/Registro/controller.js
+++ FRONT-Funme/Registro/controller.js
@@ -4,71 +4,38 @@
 'use strict';
 var app = angular.module('app', [])
 app.controller('controlador', ['$scope','$http', function($scope, $http) {
-					$scope.newPage = function() {
-						open('crearEvento.html',
-								'top=300,left=300,width=300,height=300');
-					}
 					$scope.cancelar = function() {
-						open('Calendario.html');
+						open('login.html');
 					}
-					$scope.insertUser = function() {
-
-						var usuario= {
-							email : $scope.email,
-							password : $scope.password,
-							nombre: $scope.nombre,
-							apellidos : $scope.apellidos,
-							fechanac : $scope.fechanac,
-							sexo : $scope.sexo
-						};
-			
-//						$http.post('http://localhost:8080/funme/)
-//						.success(function(data) {
-//						 alert("EXITO");
-//						}).error(function(data) {
-//						 alert("ERROR");
-//						});
-					$http({
-							    method: 'GET', 
-							    url: 'http://ip.jsontest.com/'
-							  }).success(function(data, status, headers, config) {
-							      $scope.myData=data;							      
-							  }).error(function(data, status, headers, config) {
-							      alert("Ha fallado la peticion. Estado HTTP:"+status+"     data:"+data+
-							    		  "    headers:"+headers+"      config:"+config);
-							  });
-			
-//				       $http.get('http://localhost:8080/funme/SeguroMedico/1').then(function(response) {
-//						       
-//						        $scope.content = response.data;
-//						        $scope.statuscode = response.status;
-//						        $scope.statustext = response.statustext; 
-//						       
-//				       });
-						
-
-					}
+					$scope.close = function () {
+						window.close();
+					};
 					$scope.guardarRegistro = function() {
-						var registro = {
-								email : $scope.email,
-								password : $scope.password,
-								rpassword : $scope.rpassword,
-								nombre : $scope.nombre,
-								apellidos : $scope.apellidos,
-								fecha : $scope.fecha,
-								genero : $scope.genero
-							};
 						if($scope.password != $scope.rpassword){
 							alert("Las passwords deben de coincidir");
 						}
 						else{
 							if($scope.registro.$valid){
+								var registro = {
+										email : $scope.email,
+										password : $scope.password,
+										rpassword : $scope.rpassword,
+										nombre : $scope.nombre,
+										apellidos : $scope.apellidos,
+										genero : $scope.genero
+									};
+								var mes = $scope.fecha.getMonth()+1;
+								insertarEv.fecha = $scope.fecha.getDate() + "/" + mes + "/" + $scope.fecha.getFullYear(); 
+								var posicion_x; 
+								var posicion_y; 
+								posicion_x=(screen.width/2)-(400/2); 
+								posicion_y=(screen.height/2)-(300/2); 
+
 								$http.post('http://localhost:8080/funme/Usuario',registro)
 								.success(function(data) {
-									alert("Usuario creado satisfactoriamente");
-								      location.href='../index.html';
+									window.open('popup-exito.html', this.target, 'width=400,height=300,left='+posicion_x+',top='+posicion_y+'')
 								}).error(function(data) {
-									alert('Error en el registro, inténtelo de nuevo');
+									window.open('popup-exito.html', this.target, 'width=400,height=300,left='+posicion_x+',top='+posicion_y+'')
 								});
 							}
 						}
