13f7063442911425efc22f9d711191e432a1a925
virgi680 <virgi680@gmail.com>
2016-10-25 17:41:50 +0200

    buscar evento

--- FRONT-Funme/BuscarEvento/controller.js
+++ FRONT-Funme/BuscarEvento/controller.js
@@ -26,25 +26,28 @@ app.controller('controlador', ['$scope','$http', function($scope, $http) {
 	    {
 	        name: 'provincias',
 	        source: provincias
-	    });
+	    }).on('typeahead:selected', function(event, selection) {
+			  
+			  // the second argument has the info you want
+			  $scope.lugar = selection;
+			  // clearing the selection requires a typeahead method
+			  $(this).typeahead('setQuery', '');
+			});
 	});
-	$scope.autocomplete = function(){
-		var lugar = $scope.lugar;
-		alert(lugar);
-	}
+
 	$scope.buscarEvent = function() {
-			var buscarEv = {
-					interes : $scope.interes,
-					lugar : $scope.lugar
-				};
+			if($scope.buscarEvento.$valid){
+				var buscarEv = {
+						interes : $scope.interes,
+						lugar : $scope.lugar
+					};
 
-			//alert(lugar);
-			/*$http.post('http://localhost:8080/funme/buscarEventos?interes='+buscarEv.interes+'&lugar='+buscarEv.lugar)
-			.success(function(data) {
-				location.href="../eventos.html";
-			}).error(function(data) {
-				$("#err").modal();
-			});*/
-
+				$http.post('http://localhost:8080/funme/buscarEventos?interes='+buscarEv.interes+'&lugar='+buscarEv.lugar)
+				.success(function(data) {
+					location.href="../eventos.html";
+				}).error(function(data) {
+					$("#err").modal();
+				});
+			}
 	}
 	} ]);
