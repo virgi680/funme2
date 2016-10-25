f3f8a8d268f9fc0dc0a5e08fa9d99cce81c14707
virgi680 <virgi680@gmail.com>
2016-10-25 16:06:49 +0200

    retoque de la cabecera en crear evento

--- FRONT-Funme/CrearEvento/controller.js
+++ FRONT-Funme/CrearEvento/controller.js
@@ -20,10 +20,6 @@ app.controller('controlador', ['$scope','$http', function($scope, $http) {
 							var mes = $scope.dia.getMonth()+1;
 							insertarEv.dia = $scope.dia.getDate() + "/" + mes + "/" + $scope.dia.getFullYear(); 
 							insertarEv.hora = $scope.hora.getHours() + ":" + $scope.hora.getMinutes();
-							var posicion_x; 
-							var posicion_y; 
-							posicion_x=(screen.width/2)-(400/2); 
-							posicion_y=(screen.height/2)-(180/2);
 							
 							$http.post('http://localhost:8080/funme/crearEvento?dia='+insertarEv.dia+'&hora='+insertarEv.hora+'&lugar='+insertarEv.lugar+'&nombre='+insertarEv.nombre+'&interes='+insertarEv.interes+'&descripcion='+insertarEv.descripcion+'&aforo='+insertarEv.aforo+'&email='+insertarEv.email)
 							.success(function(data) {
