152a44eff7fd37419e5116e1571800c62032ea08
virgi680 <virgi680@gmail.com>
2016-10-26 14:13:29 +0200

    control fecha crearEvento

--- FRONT-Funme/CrearEvento/controller.js
+++ FRONT-Funme/CrearEvento/controller.js
@@ -3,30 +3,32 @@
  */
 'use strict';
 var app = angular.module('app', [])
-app.controller('controlador', ['$scope','$http', function($scope, $http) {
-					$scope.cancelar = function() {
-						location.href="../index.html";
-					}
-					$scope.insertEvent = function() {
-						if($scope.crearEvento.$valid){
-							var insertarEv = {
-									lugar : $scope.lugar,
-									nombre : $scope.nombre,
-									interes : $scope.inte,
-									descripcion : $scope.desc,
-									aforo : $scope.aforo
-								};
-							var email;
-							var mes = $scope.dia.getMonth()+1;
-							insertarEv.dia = $scope.dia.getDate() + "/" + mes + "/" + $scope.dia.getFullYear(); 
-							insertarEv.hora = $scope.hora.getHours() + ":" + $scope.hora.getMinutes();
-							
-							$http.post('http://localhost:8080/funme/crearEvento?dia='+insertarEv.dia+'&hora='+insertarEv.hora+'&lugar='+insertarEv.lugar+'&nombre='+insertarEv.nombre+'&interes='+insertarEv.interes+'&descripcion='+insertarEv.descripcion+'&aforo='+insertarEv.aforo+'&email='+insertarEv.email)
-							.success(function(data) {
-								$("#ok").modal();
-							}).error(function(data) {
-								$("#err").modal();
-							});
-						}
-					}
-				} ]);
+app.controller('cont$scope.fechaActual = new Date();
+			$scope.fechaActual = $scope.fechaActual.getFullYear() + "-" + ("0" + ($scope.fechaActual.getMonth()+1)).slice(-2) + "-" + ("0" + $scope.fechaActual.getDate()).slice(-2); 
+			console.log($scope.fechaActual);
+	
+			$scope.cancelar = function() {
+				location.href="../index.html";
+			}
+			$scope.insertEvent = function() {
+				if($scope.crearEvento.$valid){
+					var insertarEv = {
+							lugar : $scope.lugar,
+							nombre : $scope.nombre,
+							interes : $scope.inte,
+							descripcion : $scope.desc,
+							aforo : $scope.aforo
+						};
+					var email;
+					insertarEv.dia = ("0" + $scope.dia.getDate()).slice(-2) + "/" + ("0" + ($scope.dia.getMonth()+1)).slice(-2)+ "/" + $scope.dia.getFullYear(); 
+					insertarEv.hora = $scope.hora.getHours() + ":" + $scope.hora.getMinutes();
+					console.log(insertarEv.dia);
+					$http.post('http://localhost:8080/funme/crearEvento?dia='+insertarEv.dia+'&hora='+insertarEv.hora+'&lugar='+insertarEv.lugar+'&nombre='+insertarEv.nombre+'&interes='+insertarEv.interes+'&descripcion='+insertarEv.descripcion+'&aforo='+insertarEv.aforo+'&email='+insertarEv.email)
+					.success(function(data) {
+						$("#ok").modal();
+					}).error(function(data) {
+						$("#err").modal();
+					});
+				}
+			}
+} ]);
